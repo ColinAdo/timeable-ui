d3137bdbea283e03171f00dfbee938da6aa84563
ColinAdo <colinadodev30@gmail.com>
2024-09-04 14:59:44 +0300

    Add metadata

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Timeable | Login",
+  description: "Login page",
+};
+
 export default function Page() {
   return (
     <main>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Timeable | Home",
+  description: "home page",
+};
+
 export default function Page() {
   return <h2>Home page</h2>;
 }
