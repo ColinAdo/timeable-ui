8c963bc871cb4a76b3094737c1635909bffafe9f
ColinAdo <colinadodev30@gmail.com>
2024-09-04 20:49:22 +0300

    Add proper navbar

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,7 +1,110 @@
+"use client";
+
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
+import { useRouter, usePathname } from "next/navigation";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { useAppDispatch, useAppSelector } from "@/redux/hook";
+import { useLogoutMutation } from "@/redux/features/authApiSlice";
+import { logout as setLogout } from "@/redux/features/authSlice";
+import { NavLink } from "@/components/common";
+import Link from "next/link";
+
 export default function Navbar() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const dispatch = useAppDispatch();
+  const { isAuthenticated } = useAppSelector((state) => state.auth);
+
+  const [logout] = useLogoutMutation();
+
+  const handlLogout = () => {
+    logout(undefined)
+      .unwrap()
+      .then(() => {
+        dispatch(setLogout());
+      })
+      .finally(() => {
+        router.push("/");
+      });
+  };
+
+  const isSelected = (path: string) => (path === pathname ? true : false);
+
+  const authLink = (isMobile: boolean) => (
+    <>
+      <NavLink
+        isSelected={isSelected("/dashboard")}
+        isMobile={isMobile}
+        href="/dashboard"
+      >
+        Dashboard
+      </NavLink>
+      <NavLink isMobile={isMobile} onClick={handlLogout}>
+        Logout
+      </NavLink>
+    </>
+  );
+  const guestLink = (isMobile: boolean) => (
+    <>
+      <NavLink
+        isSelected={isSelected("/auth/login")}
+        isMobile={isMobile}
+        href="/auth/login"
+      >
+        Login
+      </NavLink>
+      <NavLink
+        isSelected={isSelected("/auth/register")}
+        isMobile={isMobile}
+        href="/auth/register"
+      >
+        Register
+      </NavLink>
+    </>
+  );
+
   return (
-    <nav>
-      <h2>Navbar</h2>
-    </nav>
+    <Disclosure as="nav" className="bg-gray-800">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon
+                aria-hidden="true"
+                className="block h-6 w-6 group-data-[open]:hidden"
+              />
+              <XMarkIcon
+                aria-hidden="true"
+                className="hidden h-6 w-6 group-data-[open]:block"
+              />
+            </DisclosureButton>
+          </div>
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex flex-shrink-0 items-center">
+              <NavLink href="/" isBanner>
+                Timeable
+              </NavLink>
+            </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                {isAuthenticated ? authLink(false) : guestLink(false)}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-2 pb-3 pt-2">
+          {isAuthenticated ? authLink(true) : guestLink(true)}
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
   );
 }
--- components/utils/SetUp.tsx
+++ components/utils/SetUp.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { useVerify } from "@/hooks";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function SetUp() {
+  useVerify();
   return <ToastContainer />;
 }
