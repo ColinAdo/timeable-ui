9ee4f8bd94d9754a7d047010a812c6b4efa4a677
ColinAdo <colinadodev30@gmail.com>
2024-09-04 13:30:10 +0300

    Add refactored registration code

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,19 +1,7 @@
-"use client";
-
-import { Spinner } from "@/components/common";
-import { useRegister } from "@/hooks";
 import Link from "next/link";
+import { RegisterForm } from "@/components/forms";
 
 export default function Page() {
-  const {
-    username,
-    email,
-    password,
-    re_password,
-    isLoading,
-    onChange,
-    onSubmit,
-  } = useRegister();
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -28,97 +16,7 @@ export default function Page() {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" onSubmit={onSubmit}>
-          <div>
-            <label
-              htmlFor="username"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Username
-            </label>
-            <div className="mt-2">
-              <input
-                id="username"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                name="username"
-                type="text"
-                onChange={onChange}
-                value={username}
-                required
-              />
-            </div>
-          </div>
-
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                name="email"
-                type="email"
-                onChange={onChange}
-                value={email}
-                required
-              />
-            </div>
-          </div>
-
-          <div>
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Password
-            </label>
-            <div className="mt-2">
-              <input
-                id="password"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                name="password"
-                type="password"
-                value={password}
-                onChange={onChange}
-                required
-              />
-            </div>
-          </div>
-
-          <div>
-            <label
-              htmlFor="re_password"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Confirm password
-            </label>
-            <div className="mt-2">
-              <input
-                id="re_password"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                name="re_password"
-                type="password"
-                onChange={onChange}
-                value={re_password}
-                required
-              />
-            </div>
-          </div>
-
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              {isLoading ? <Spinner sm /> : "Sign up"}
-            </button>
-          </div>
-        </form>
-
+        <RegisterForm />
         <p className="mt-10 text-center text-sm text-gray-500">
           Already have an account?{" "}
           <Link
