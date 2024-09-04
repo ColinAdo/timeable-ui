c4cf3bad52cd34c57d1a0fdd7575a1a9b2b9508c
ColinAdo <colinadodev30@gmail.com>
2024-09-04 20:54:07 +0300

    Add proper footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,7 +1,15 @@
 export default function Footer() {
   return (
     <footer>
-      <h2>Footer</h2>
+      <div className="bg-gray-300 h-16 ">
+        <div className="h-full px-2">
+          <div className="flex justify-center items-center h-full">
+            <p className="text-gray-500 text-xl">
+              &copy; 2024 Inc all rights reserved
+            </p>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
