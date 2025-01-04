425771eb6e905a30273c6ce9957272ec00f210a1
ColinAdo <colinadodev30@gmail.com>
2025-01-04 15:34:54 +0300

    Add auth route

---
+++ app/auth/(acccount)/login/page.tsx
@@ -0,0 +1,19 @@
+import { GraduationCap } from "lucide-react";
+import { AuthTabs } from "@/components/auth";
+
+export default function Page() {
+    return (
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm mb-0">
+                <GraduationCap className="mx-auto h-10 w-auto" />
+                <h2 className="mt-1 text-center text-2xl dark:text-white font-mono font-bold leading-9 tracking-tight text-gray-900">
+                    Timeable
+                </h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <AuthTabs />
+            </div>
+        </div>
+    );
+}
