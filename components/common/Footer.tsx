5e2e3e2fa5b5c617e3b4c49c5f4bf6c45d822e83
ColinAdo <colinadodev30@gmail.com>
2025-01-04 14:07:59 +0300

    Add proper footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-100 h-16">
+    <footer className="h-16">
       <div className="h-full px-2">
         <div className="flex items-center justify-center h-full">
           <p className="text-gray-500 text-xs">
-            &copy; 2024 Timeable, Inc. All rights reserved.
+            &copy; 2024 Monie, Inc. All rights reserved.
           </p>
         </div>
       </div>
