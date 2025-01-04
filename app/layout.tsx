6a14b0c3713be18eb900ca1d5074440a2001b2cc
ColinAdo <colinadodev30@gmail.com>
2025-01-04 13:54:27 +0300

    Add proper app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "@/styles/globals.css";
+import type { Metadata } from "next";
 
+// import { Toaster } from "sonner";
 import Provider from "@/redux/provider";
-import { Navbar, Footer } from "@/components/common";
-import { SetUp } from "@/components/utils";
+import { Inter } from "next/font/google";
+// import { Setup } from "@/components/utils";
+import { Footer } from "@/components/common";
+// import { ThemeProvider } from "@/components/common/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Monie",
-  description: "Monie is an app to manage your finances",
+  title: "Timeable",
+  description: "Timeable is an app that allows you tp generate university academic timetables",
 };
 
 export default function RootLayout({
@@ -22,10 +24,17 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Provider>
-          <SetUp />
-          <Navbar />
-          <div className="mx-auto max-h-7xl px-2 sm:px-6 lg:px-8 my-8">
+          {/* <Setup /> */}
+          <div className="mx-auto max-h-7xl px-2 sm:px-6 lg:px-8 mt-6">
+            {/* <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            > */}
             {children}
+            {/* </ThemeProvider> */}
+            {/* <Toaster position="top-center" richColors /> */}
           </div>
           <Footer />
         </Provider>
