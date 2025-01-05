adc9b097086b7d50bbae0d0a5cacf429fcca589d
ColinAdo <colinadodev30@gmail.com>
2025-01-05 15:01:40 +0300

    Add proper nav bar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,23 @@
 import { RequireAuth } from "@/components/utils";
+import { Navbar } from "@/components/common";
+// import { WebSocketProvider } from "@/hooks/WebSocketContext";
 
 interface Props {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: Props) {
-  return <RequireAuth>{children}</RequireAuth>;
+  return (
+    <RequireAuth>
+      {/* <WebSocketProvider> */}
+      <Navbar />
+      <div className="flex">
+        {/* <div className="hidden md:block">
+            <Sidebar />
+          </div> */}
+        <div className="p-5 w-full max-w-[1140px]">{children}</div>
+      </div>
+      {/* </WebSocketProvider> */}
+    </RequireAuth>
+  );
 }
--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,25 +1,38 @@
 "use client";
 
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-} from "@headlessui/react";
-import { usePathname } from "next/navigation";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import { useAppDispatch, useAppSelector } from "@/redux/hook";
-import { useLogoutMutation } from "@/redux/features/authApiSlice";
+import Link from "next/link";
+import { GraduationCap } from "lucide-react";
+import { useAppDispatch } from "@/redux/hook";
+import { ThemeToggler } from "@/components/common";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { logout as setLogout } from "@/redux/features/authSlice";
-import { NavLink } from "@/components/common";
+import {
+  LogOut,
+  BadgePlus,
+  UserRound,
+  DiamondPlus,
+  BadgeDollarSign
+} from "lucide-react";
+import {
+  useLogoutMutation,
+  useRetrieveUserQuery,
+} from "@/redux/features/authApiSlice";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Navbar() {
-  const pathname = usePathname();
   const dispatch = useAppDispatch();
-  const { isAuthenticated } = useAppSelector((state) => state.auth);
-
   const [logout] = useLogoutMutation();
+  const { data: user } = useRetrieveUserQuery();
 
-  const handlLogout = () => {
+  const handleLogout = () => {
     logout(undefined)
       .unwrap()
       .then(() => {
@@ -27,79 +40,65 @@ export default function Navbar() {
       });
   };
 
-  const isSelected = (path: string) => (path === pathname ? true : false);
-
-  const authLink = (isMobile: boolean) => (
-    <>
-      <NavLink
-        isSelected={isSelected("/dashboard")}
-        isMobile={isMobile}
-        href="/dashboard"
-      >
-        Dashboard
-      </NavLink>
-      <NavLink isMobile={isMobile} onClick={handlLogout}>
-        Logout
-      </NavLink>
-    </>
-  );
-  const guestLink = (isMobile: boolean) => (
-    <>
-      <NavLink
-        isSelected={isSelected("/auth/login")}
-        isMobile={isMobile}
-        href="/auth/login"
-      >
-        Login
-      </NavLink>
-      <NavLink
-        isSelected={isSelected("/auth/register")}
-        isMobile={isMobile}
-        href="/auth/register"
-      >
-        Register
-      </NavLink>
-    </>
-  );
-
   return (
-    <Disclosure as="nav" className="bg-gray-800">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-              <span className="absolute -inset-0.5" />
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon
-                aria-hidden="true"
-                className="block h-6 w-6 group-data-[open]:hidden"
-              />
-              <XMarkIcon
-                aria-hidden="true"
-                className="hidden h-6 w-6 group-data-[open]:block"
-              />
-            </DisclosureButton>
-          </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex flex-shrink-0 items-center">
-              <NavLink href="/" isBanner>
-                Timeable
-              </NavLink>
-            </div>
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
-                {isAuthenticated ? authLink(false) : guestLink(false)}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className="text-black px-5 flex justify-between">
+      <Link href="/dashboard">
+        <GraduationCap className="dark:text-white" />
+      </Link>
 
-      <DisclosurePanel className="sm:hidden">
-        <div className="space-y-1 px-2 pb-3 pt-2">
-          {isAuthenticated ? authLink(true) : guestLink(true)}
-        </div>
-      </DisclosurePanel>
-    </Disclosure>
+      <div className="flex item-center">
+        <DropdownMenu>
+          <DropdownMenuTrigger className="focus:outline-none">
+            <Avatar>
+              <AvatarFallback className="text-black bg-slate-300 font-bold">
+                {user?.username[0]}
+              </AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>{user?.username}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href="/" className="flex justify-between">
+                <UserRound className="h-[1.2rem] w-[1.2rem]" />
+                <span className="ml-2">Profile</span>
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link
+                href="/dashboard/create/account"
+                className="flex justify-between"
+              >
+                <BadgePlus className="h-[1.2rem] w-[1.2rem]" />
+                <span className="ml-2">Create</span>
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link
+                href="/dashboard/create/transaction"
+                className="flex justify-between"
+              >
+                <DiamondPlus className="h-[1.2rem] w-[1.2rem]" />
+                <span className="ml-2">Transact</span>
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link
+                href="/dashboard/transactions"
+                className="flex justify-between"
+              >
+                <BadgeDollarSign className="h-[1.2rem] w-[1.2rem]" />
+                <span className="ml-2">Transation</span>
+              </Link>
+            </DropdownMenuItem>
+            <ThemeToggler />
+            <DropdownMenuItem>
+              <LogOut className="h-[1.2rem] w-[1.2rem]" />
+              <button onClick={handleLogout}>Logout</button>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
   );
 }
