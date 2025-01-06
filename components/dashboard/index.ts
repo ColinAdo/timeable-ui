700241e29c91018fdd409ae53f7e9170d2974642
ColinAdo <colinadodev30@gmail.com>
2025-01-06 11:35:59 +0300

    Add index for pretty export

---
+++ components/dashboard/index.ts
@@ -0,0 +1,4 @@
+export { default as CardItem } from "./CardItem";
+export { default as PageTitle } from "./PageTitle";
+export { default as AnalyticBarChart } from "./AnalyticBarChart";
+export { default as AnalyticLineChart } from "./AnalyticLineChart";
