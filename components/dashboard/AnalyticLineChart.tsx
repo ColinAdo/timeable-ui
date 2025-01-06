07206abf2f6756fe310c1838c5b4c145fe6e4f72
ColinAdo <colinadodev30@gmail.com>
2025-01-06 11:35:37 +0300

    Add analytic components

---
+++ components/dashboard/AnalyticLineChart.tsx
@@ -0,0 +1,121 @@
+"use client";
+
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    Tooltip,
+    CartesianGrid,
+    ResponsiveContainer,
+} from "recharts";
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+
+import { useState } from "react";
+import { ChevronsLeft, ChevronsRight } from "lucide-react";
+
+const data = [
+    {
+        name: 'Page A',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Page B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: 'Page C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: 'Page D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: 'Page E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: 'Page F',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: 'Page G',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    },
+];
+
+export default function AnalyticCard() {
+    const [year, setYear] = useState(new Date().getFullYear());
+
+    const handleYearChange = (newYear: number) => {
+        setYear(newYear);
+    };
+
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Transaction Analytics</CardTitle>
+                <CardDescription>
+                    Line Chart showing analytics for your monthly transactions in {year}
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div>
+                    <button
+                        disabled={year <= 2024}
+                        onClick={() => handleYearChange(year - 1)}
+                        className={year <= 2024 ? "disable" : ""}
+                    >
+                        <ChevronsLeft
+                            className={`h-4 w-4 ${year <= 2024 ? "text-gray-400" : "text-black dark:text-white"}`}
+                        />
+                    </button>
+
+                    <span className="p-2 font-bold">{year}</span>
+                    <button
+                        disabled={year === 2025}
+                        onClick={() => handleYearChange(year + 1)}
+                        className={year === 2025 ? "disable" : ""}
+                    >
+                        <ChevronsRight
+                            className={`h-4 w-4 ${year === 2025 ? "text-gray-400" : "text-black dark:text-white"}`}
+                        />
+                    </button>
+                </div>
+                <div style={{ width: "100%", height: 300 }}>
+                    <ResponsiveContainer>
+                        <LineChart width={1100} height={300} data={data}>
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Line type="monotone" dataKey="amount" stroke="#8884d8" />
+                            <CartesianGrid stroke="#ccc" />
+                        </LineChart>
+                    </ResponsiveContainer>
+                </div>
+            </CardContent>
+        </Card>
+    );
+}
