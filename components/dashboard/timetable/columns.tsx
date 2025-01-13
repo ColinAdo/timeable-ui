c77e633c26fb1495699e17e941aa2616e457a07f
ColinAdo <colinadodev30@gmail.com>
2025-01-13 12:16:59 +0300

    Ad data

--- components/dashboard/timetable/columns.tsx
+++ components/dashboard/timetable/columns.tsx
@@ -3,10 +3,10 @@
 import { ColumnDef } from "@tanstack/react-table"
 
 import { Badge } from "@/components/ui/badge"
-import { Checkbox } from "@/registry/new-york/ui/checkbox"
+import { Checkbox } from "@/components/ui/checkbox"
 
-import { labels, priorities, statuses } from "../data/data"
-import { Task } from "../data/schema"
+import { labels, priorities, statuses } from "@/data/data"
+import { Task } from "@/lib/schemas"
 import { DataTableColumnHeader } from "./data-table-column-header"
 import { DataTableRowActions } from "./data-table-row-actions"
 
