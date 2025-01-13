e541c95179ed386491c29b0afe36e8b19a566ea6
ColinAdo <colinadodev30@gmail.com>
2025-01-13 12:27:44 +0300

    Add data-table-row-actions component

--- components/dashboard/timetable/columns.tsx
+++ components/dashboard/timetable/columns.tsx
@@ -8,7 +8,7 @@ import { Checkbox } from "@/components/ui/checkbox"
 import { labels, priorities, statuses } from "@/data/data"
 import { Task } from "@/lib/schemas"
 import { DataTableColumnHeader } from "@/components/dashboard/timetable/data-table-column-header"
-import { DataTableRowActions } from "./data-table-row-actions"
+import { DataTableRowActions } from "@/components/dashboard/timetable/data-table-row-actions"
 
 export const columns: ColumnDef<Task>[] = [
     {
