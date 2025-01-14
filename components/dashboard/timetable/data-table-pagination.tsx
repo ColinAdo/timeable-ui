f2dfce8ea411bfa105df890534a4b68ae18f0708
ColinAdo <colinadodev30@gmail.com>
2025-01-14 20:38:56 +0300

    Add proper import

--- components/dashboard/timetable/data-table-pagination.tsx
+++ components/dashboard/timetable/data-table-pagination.tsx
@@ -1,4 +1,5 @@
 import { Table } from "@tanstack/react-table"
+import { Button } from "@/components/ui/button"
 import {
     ChevronLeft,
     ChevronRight,
@@ -6,7 +7,6 @@ import {
     ChevronsRight,
 } from "lucide-react"
 
-import { Button } from "@/components/ui/button"
 import {
     Select,
     SelectContent,
