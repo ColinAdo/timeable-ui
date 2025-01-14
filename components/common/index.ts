babdb344d4d3808d49fa83f69937004c12242d88
ColinAdo <colinadodev30@gmail.com>
2025-01-14 20:34:41 +0300

    Add proper import

--- components/common/index.ts
+++ components/common/index.ts
@@ -1,9 +1,9 @@
-export { default as Navbar } from "./Navbar";
-export { default as Sidebar } from "./Sidebar";
-export { default as Footer } from "./Footer";
-export { default as Spinner } from "./Spinner";
 export { default as List } from "./List";
+export { default as Navbar } from "./Navbar";
+export { default as Footer } from "./Footer";
+export { default as Sidebar } from "./Sidebar";
+export { default as Spinner } from "./Spinner";
+export { default as Pricing } from "./Pricing";
+export { default as ThemeToggler } from "./ThemeToggler";
 export { default as SocialButton } from "./SocialButton";
 export { default as SocialButtons } from "./SocialButtons";
-export { default as ThemeToggler } from "./ThemeToggler";
-export { default as Pricing } from "./Pricing";
