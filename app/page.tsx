4b40c75d3c0550a1d08230aeea090ae4d356e285
ColinAdo <colinadodev30@gmail.com>
2025-01-20 18:49:53 +0300

    Add proper layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,37 @@
-import Link from "next/link";
-import type { Metadata } from "next";
+import { AboutSectionOne } from "@/components/home";
+import { AboutSectionTwo } from "@/components/home";
+import Blog from "@/components/home/Blog/index";
+import Brands from "@/components/home/Brands/index";
+import { ScrollUp } from "@/components/home/";
+import Contact from "@/components/home/Contact";
+import Features from "@/components/home/Features";
+import { Hero } from "@/components/home";
+import Pricing from "@/components/home/Pricing";
+import Testimonials from "@/components/home/Testimonials";
+import { Video } from "@/components/home";
+import { Metadata } from "next";
+import Header from "@/components/home/Header";
 
 export const metadata: Metadata = {
   title: "Timeable | Home",
   description: "home page",
 };
 
-export default function Page() {
+export default function Home() {
   return (
-    <main className="bg-white dark:bg-zinc-950">
-      <div className="relative isolate px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="text-center">
-            <h1 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-6xl">
-              Automatic timetable Generator
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              Our Timeatable is a timetable Generator which simplifies
-              scheduling for higher institutions by transforming your uploaded
-              Excel file into a fully functional timetable. This web app
-              automatically processes class schedules, lecturer availability,
-              and room allocations, ensuring efficient and conflict-free
-              timetables.
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <Link
-                href="/auth/login"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Get started <span aria-hidden="true">&rarr;</span>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div
-          aria-hidden="true"
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-        >
-          <div
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-          />
-        </div>
-      </div>
-    </main>
+    <>
+      <Header />
+      <ScrollUp />
+      <Hero />
+      <Features />
+      <Video />
+      <Brands />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+      <Testimonials />
+      <Pricing />
+      <Blog />
+      <Contact />
+    </>
   );
 }
