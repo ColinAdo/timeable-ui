3d9c5237913fbcd3a2735351fd4c3b22be4fa5c8
ColinAdo <colinadodev30@gmail.com>
2025-01-21 12:30:19 +0300

    Deleted unwanted components

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
-import { AboutSectionOne } from "@/components/home";
-import { AboutSectionTwo } from "@/components/home";
+import { Hero, Video } from "@/components/home";
+import { ScrollUp } from "@/components/home/";
 import Blog from "@/components/home/Blog/index";
 import Brands from "@/components/home/Brands/index";
-import { ScrollUp } from "@/components/home/";
 import Contact from "@/components/home/Contact";
 import Features from "@/components/home/Features";
-import { Hero } from "@/components/home";
+import Header from "@/components/home/Header";
 import Pricing from "@/components/home/Pricing";
 import Testimonials from "@/components/home/Testimonials";
-import { Video } from "@/components/home";
 import { Metadata } from "next";
-import Header from "@/components/home/Header";
 
 export const metadata: Metadata = {
   title: "Timeable | Home",
@@ -26,8 +23,6 @@ export default function Home() {
       <Features />
       <Video />
       <Brands />
-      <AboutSectionOne />
-      <AboutSectionTwo />
       <Testimonials />
       <Pricing />
       <Blog />
--- components/home/index.ts
+++ components/home/index.ts
@@ -1,5 +1,3 @@
-export { default as AboutSectionOne } from './AboutSectionOne';
-export { default as AboutSectionTwo } from './AboutSectionTwo';
 export { default as ScrollUp } from './ScrollUp';
 export { default as Hero } from './Hero';
 export { default as Video } from './Video';
