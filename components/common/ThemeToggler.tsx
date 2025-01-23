b762d6ab79fe597cd326acdf2f997b8cb0834671
ColinAdo <colinadodev30@gmail.com>
2025-01-23 11:02:25 +0300

    Add proper import

--- components/common/ThemeToggler.tsx
+++ components/common/ThemeToggler.tsx
@@ -5,8 +5,8 @@ import { Moon, Sun } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
     DropdownMenu,
-    DropdownMenuContent,
     DropdownMenuItem,
+    DropdownMenuContent,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
