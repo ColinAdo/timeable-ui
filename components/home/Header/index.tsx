b6ac7849f20d429b96834fed7f64761837a28957
ColinAdo <colinadodev30@gmail.com>
2025-01-27 10:05:59 +0300

    Add condiional navbar display link

--- components/home/Header/index.tsx
+++ components/home/Header/index.tsx
@@ -46,6 +46,7 @@ export default function Header() {
   };
 
   const usePathName = usePathname();
+  const isLoginPage = usePathName === "/auth/login";
 
   console.log("usePathName", usePathName);
 
@@ -70,40 +71,41 @@ export default function Header() {
             </div>
             <div className="flex w-full items-center justify-between">
               <div>
-                <button
-                  onClick={navbarToggleHandler}
-                  id="navbarToggler"
-                  aria-label="Mobile Menu"
-                  className="absolute right-4 top-1/2 block translate-y-[-50%] rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden"
-                >
-                  <span
-                    className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${navbarOpen ? " top-[7px] rotate-45" : " "
-                      }`}
-                  />
-                  <span
-                    className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${navbarOpen ? "opacity-0 " : " "
-                      }`}
-                  />
-                  <span
-                    className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${navbarOpen ? " top-[-8px] -rotate-45" : " "
-                      }`}
-                  />
-                </button>
-                <nav
-                  id="navbarCollapse"
-                  className={`navbar dark:bg-neutral-950 absolute right-0 z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-white px-6 py-4 duration-300 dark:border-body-color/20 dark:bg-dark lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${navbarOpen
-                    ? "visibility top-full opacity-100"
-                    : "invisible top-[120%] opacity-0"
-                    }`}
-                >
-                  <ul className="block lg:flex lg:space-x-12">
-                    {menuData.map((menuItem, index) => (
-                      <li key={index} className="group relative">
-                        {menuItem.path ? (
-                          <>
-                            {usePathName === "/auth/login" ? (
-                              <h2></h2>
-                            ) : (
+                {isLoginPage ? (
+                  <h2 className="hidden"></h2>
+
+                ) : (
+                  <>
+                    <button
+                      onClick={navbarToggleHandler}
+                      id="navbarToggler"
+                      aria-label="Mobile Menu"
+                      className="absolute right-4 top-1/2 block translate-y-[-50%] rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden"
+                    >
+                      <span
+                        className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${navbarOpen ? " top-[7px] rotate-45" : " "
+                          }`}
+                      />
+                      <span
+                        className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${navbarOpen ? "opacity-0 " : " "
+                          }`}
+                      />
+                      <span
+                        className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${navbarOpen ? " top-[-8px] -rotate-45" : " "
+                          }`}
+                      />
+                    </button>
+                    <nav
+                      id="navbarCollapse"
+                      className={`navbar dark:bg-neutral-950 absolute right-0 z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-white px-6 py-4 duration-300 dark:border-body-color/20 dark:bg-dark lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${navbarOpen
+                        ? "visibility top-full opacity-100"
+                        : "invisible top-[120%] opacity-0"
+                        }`}
+                    >
+                      <ul className="block lg:flex lg:space-x-12">
+                        {menuData.map((menuItem, index) => (
+                          <li key={index} className="group relative">
+                            {menuItem.path ? (
                               <Link
                                 href={menuItem.path}
                                 onClick={(e) => handleClick(e, `${menuItem.path}`)}
@@ -115,33 +117,33 @@ export default function Header() {
                               >
                                 {menuItem.title}
                               </Link>
-                            )}
-                          </>
-                        ) : (
-                          <>
-                            <p
-                              onClick={() => handleSubmenu(index)}
-                              className="flex cursor-pointer items-center justify-between py-2 text-base text-dark group-hover:text-primary dark:text-white/70 dark:group-hover:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6"
-                            >
-                              {menuItem.title}
-                              <span className="pl-3">
-                                <svg width="25" height="24" viewBox="0 0 25 24">
-                                  <path
-                                    fillRule="evenodd"
-                                    clipRule="evenodd"
-                                    d="M6.29289 8.8427C6.68342 8.45217 7.31658 8.45217 7.70711 8.8427L12 13.1356L16.2929 8.8427C16.6834 8.45217 17.3166 8.45217 17.7071 8.8427C18.0976 9.23322 18.0976 9.86639 17.7071 10.2569L12 15.964L6.29289 10.2569C5.90237 9.86639 5.90237 9.23322 6.29289 8.8427Z"
-                                    fill="currentColor"
-                                  />
-                                </svg>
-                              </span>
-                            </p>
+                            ) : (
+                              <>
+                                <p
+                                  onClick={() => handleSubmenu(index)}
+                                  className="flex cursor-pointer items-center justify-between py-2 text-base text-dark group-hover:text-primary dark:text-white/70 dark:group-hover:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6"
+                                >
+                                  {menuItem.title}
+                                  <span className="pl-3">
+                                    <svg width="25" height="24" viewBox="0 0 25 24">
+                                      <path
+                                        fillRule="evenodd"
+                                        clipRule="evenodd"
+                                        d="M6.29289 8.8427C6.68342 8.45217 7.31658 8.45217 7.70711 8.8427L12 13.1356L16.2929 8.8427C16.6834 8.45217 17.3166 8.45217 17.7071 8.8427C18.0976 9.23322 18.0976 9.86639 17.7071 10.2569L12 15.964L6.29289 10.2569C5.90237 9.86639 5.90237 9.23322 6.29289 8.8427Z"
+                                        fill="currentColor"
+                                      />
+                                    </svg>
+                                  </span>
+                                </p>
 
-                          </>
-                        )}
-                      </li>
-                    ))}
-                  </ul>
-                </nav>
+                              </>
+                            )}
+                          </li>
+                        ))}
+                      </ul>
+                    </nav>
+                  </>
+                )}
               </div>
               <div className="flex items-center justify-end pr-16 lg:pr-0">
                 <Link
@@ -156,7 +158,7 @@ export default function Header() {
                 >
                   Sign Up
                 </Link>
-                <div>
+                <div className={`${isLoginPage}` ? "ml-12" : ""}>
                   <ThemeToggler />
                 </div>
               </div>
