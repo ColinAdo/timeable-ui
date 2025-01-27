6aa89e7df079cd6a03a574ce78e45449cf61784e
ColinAdo <colinadodev30@gmail.com>
2025-01-27 10:13:49 +0300

    Add proper function

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-const Hero = () => {
+export default function Hero() {
     return (
         <>
             <section
@@ -44,5 +44,3 @@ const Hero = () => {
         </>
     );
 };
-
-export default Hero;
