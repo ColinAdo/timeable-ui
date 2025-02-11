f670161ebf137a4f777218484c022270e7f47e75
ColinAdo <colinadodev30@gmail.com>
2025-02-11 17:14:40 +0300

    Rmoved unwanted comment

--- app/password-reset/page.tsx
+++ app/password-reset/page.tsx
@@ -1,23 +1,5 @@
+import { PasswordResetForm } from "@/components/forms";
 import { GraduationCap } from "lucide-react";
-import { PasswordResetForm, LoginForm } from "@/components/forms";
-import { AuthTabs } from "@/components/auth";
-
-// export default function Page() {
-//   return (
-//     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-//       <div className="sm:mx-auto sm:w-full sm:max-w-sm mb-0">
-//         <GraduationCap className="mx-auto h-10 w-auto" />
-//         <h2 className="mt-1 text-center dark:text-white text-2xl font-mono font-bold leading-9 tracking-tight text-gray-900">
-//           Timeable
-//         </h2>
-//       </div>
-
-//       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-//         <PasswordResetForm />
-//       </div>
-//     </div>
-//   );
-// }
 
 export default function Page() {
   return (
