95a3574991f87cb99d79ecd98795d9eea35e850d
ColinAdo <colinadodev30@gmail.com>
2025-02-12 09:01:43 +0300

    Add proper background color

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -11,16 +11,16 @@ export default function NotFound() {
   };
 
   return (
-    <main className="flex h-full flex-col py-52 items-center justify-center gap-2">
+    <main className="flex h-full bg-black/[0.96] bg-grid-white/[0.02] flex-col py-52 items-center justify-center gap-2">
       <Frown className="w-10 text-gray-400" />
-      <h2 className="text-xl font-semibold">404 Not Found</h2>
-      <p>
+      <h2 className="text-xl font-semibold text-gray-200">404 Not Found</h2>
+      <p className="text-gray-200">
         The page you are looking for does not exist. Please check the URL or go
         back.
       </p>
       <button
         onClick={handleGoBack}
-        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+        className="bg-gradient-to-r p-2 from-purple-500 rounded to-pink-500 hover:from-purple-600 hover:to-pink-600 text-white"
       >
         Go Back
       </button>
