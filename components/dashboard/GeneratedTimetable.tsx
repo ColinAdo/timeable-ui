cc3705ddaf3d157be0aa11d3fe36f7af6f23ee76
ColinAdo <colinadodev30@gmail.com>
2025-02-12 09:03:59 +0300

    Add generated timetable list component

---
+++ components/dashboard/GeneratedTimetable.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { CardItem } from "@/components/dashboard/";
+import { useState } from "react";
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+interface Props {
+    name: string;
+    email: string;
+    amount: string;
+}
+
+const SalesData: Props[] = [
+    { name: "Timetable One", email: "annie@gmail.com", amount: "12/Jan/2024" },
+    { name: "Timetable One", email: "maggie@gmail.com", amount: "12/Jan/2024" },
+    { name: "Timetable Two", email: "george@gmail.com", amount: "12/Jan/2024" },
+    { name: "Timetable Three", email: "coco@gmail.com", amount: "12/Jan/2024" },
+    { name: "Timetable Four", email: "boo@gmail.com", amount: "12/Jan/2024" },
+    { name: "Timetable Five", email: "sasha@gmail.com", amount: "12/Jan/2024" },
+];
+
+export default function AnalyticCard() {
+    const [year, setYear] = useState(new Date().getFullYear());
+
+    const handleYearChange = (newYear: number) => {
+        setYear(newYear);
+    };
+
+    return (
+        <Card className="bg-black/[0.96] bg-grid-white[0.02] border-purple-400">
+            <CardHeader>
+                <CardTitle>Transaction Analytics</CardTitle>
+                <CardDescription>
+                    Line Chart showing analytics for your monthly transactions in {year}
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                {SalesData.map((d, i) => (
+                    <CardItem
+                        key={i}
+                        accountName={d.name}
+                        email={d.email}
+                        amount={d.amount}
+                        username="Colin"
+                        description="Some description"
+                    />
+                ))}
+                <Link className="flex justify-end text-blue-400" href="#">
+                    See all
+                </Link>
+            </CardContent>
+        </Card>
+    );
+}
