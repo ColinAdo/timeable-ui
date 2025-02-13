8ac28004b71daed0413af2f1fcf0be54cc705dca
ColinAdo <colinadodev30@gmail.com>
2025-02-13 10:54:32 +0300

    Add table types

---
+++ types/exports.ts
@@ -0,0 +1,12 @@
+export type RowData = {
+    id: string
+    unit_code: string
+    unit_name: string
+    day: string
+    time: string
+    lecturer: string
+    campus: string
+    mode: string
+    room: string
+    group: string
+}
