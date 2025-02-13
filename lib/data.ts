aff53ef2e749147b11a946d64f896fda8ac4990a
ColinAdo <colinadodev30@gmail.com>
2025-02-13 10:54:51 +0300

    Add timetable data

---
+++ lib/data.ts
@@ -0,0 +1,31 @@
+import { RowData } from "@/types/exports"
+
+export const initialData: RowData[] = [
+    {
+        id: "1",
+        unit_code: "CS101",
+        unit_name: "Introduction to Computer Science",
+        day: "Monday",
+        time: "09:00 - 11:00",
+        lecturer: "Dr. Smith",
+        campus: "Main",
+        mode: "In-person",
+        room: "A101",
+        group: "Group A",
+    },
+    {
+        id: "2",
+        unit_code: "CS102",
+        unit_name: "Data Structures and Algorithms",
+        day: "Tuesday",
+        time: "13:00 - 15:00",
+        lecturer: "Prof. Johnson",
+        campus: "South",
+        mode: "Online",
+        room: "Virtual",
+        group: "Group B",
+    },
+    // Add more initial data as needed
+]
+
+export const allColumns = ["unit_code", "unit_name", "day", "time", "lecturer", "campus", "mode", "room", "group"]
