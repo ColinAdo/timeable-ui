553d998a448f639a465ef28742e0d177112c57da
ColinAdo <colinadodev30@gmail.com>
2025-02-16 22:27:42 +0300

    Ad proper import

--- components/dashboard/index.ts
+++ components/dashboard/index.ts
@@ -1,8 +1,8 @@
 export { default as CardItem } from "./CardItem";
 export { default as PageTitle } from "./PageTitle";
+export { default as UploadFile } from "./UploadFile";
 export { default as FormDialog } from "./FormDialog";
 export { default as AnalyticBarChart } from "./AnalyticBarChart";
 export { default as AnalyticLineChart } from "./AnalyticLineChart";
 export { default as CreateTimetableForm } from "./CreateTimetableForm";
 export { default as GeneratedTimetable } from "./GeneratedTimetable";
-export { default as UploadFile } from "./UploadFile";
