f8f9e0bff75c84a024634aba42e43fb1319d579e
ColinAdo <colinadodev30@gmail.com>
2025-02-23 21:37:59 +0300

    Add proper import

--- components/common/index.ts
+++ components/common/index.ts
@@ -4,6 +4,6 @@ export { default as Footer } from "./Footer";
 export { default as Sidebar } from "./Sidebar";
 export { default as Spinner } from "./Spinner";
 export { default as Pricing } from "./Pricing";
+export { default as SectionTitle } from "./SectionTitle";
 export { default as SocialButton } from "./SocialButton";
 export { default as SocialButtons } from "./SocialButtons";
-export { default as SectionTitle } from "./SectionTitle";
