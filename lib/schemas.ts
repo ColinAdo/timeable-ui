29a8ed4a738b5ec8ddaeec6ecfce9832c2c59e04
ColinAdo <colinadodev30@gmail.com>
2025-02-24 23:11:16 +0300

    Deleed unwanted comments

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -64,9 +64,6 @@ export const createTimetableSchema = z.object({
     prompt: z.string().optional(),
 });
 
-
-
-// We're keeping a simple non-relational schema here.
 // IRL, you will have a schema for your data models.
 export const taskSchema = z.object({
     id: z.string(),
