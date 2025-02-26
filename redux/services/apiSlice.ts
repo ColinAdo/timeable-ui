1e563cee3622f0a869a40e229d4ed2ac9d328561
ColinAdo <colinadodev30@gmail.com>
2025-02-26 23:54:16 +0300

    Add proper import

--- redux/services/apiSlice.ts
+++ redux/services/apiSlice.ts
@@ -1,11 +1,11 @@
+import { Mutex } from "async-mutex";
+import { logout, setAuth } from "../features/authSlice";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type {
   BaseQueryFn,
   FetchArgs,
   FetchBaseQueryError,
 } from "@reduxjs/toolkit/query";
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { Mutex } from "async-mutex";
-import { logout, setAuth } from "../features/authSlice";
 
 // create a new mutex
 const mutex = new Mutex();
