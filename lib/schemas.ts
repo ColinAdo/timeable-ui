a1c8e9ab05ddedef197a5e69f35edec19091a48b
ColinAdo <colinadodev30@gmail.com>
2025-02-26 23:48:17 +0300

    Removed unwanted code

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -64,13 +64,5 @@ export const createTimetableSchema = z.object({
     prompt: z.string().optional(),
 });
 
-// IRL, you will have a schema for your data models.
-export const taskSchema = z.object({
-    id: z.string(),
-    title: z.string(),
-    status: z.string(),
-    label: z.string(),
-    priority: z.string(),
-})
 
 export type Task = z.infer<typeof taskSchema>
