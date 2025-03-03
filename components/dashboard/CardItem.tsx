4aea80af8fc0bbd3f731279fce4db1272de46a50
ColinAdo <colinadodev30@gmail.com>
2025-03-03 23:01:25 +0300

    Add dropdown menu

--- components/dashboard/CardItem.tsx
+++ components/dashboard/CardItem.tsx
@@ -1,5 +1,8 @@
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import Link from "next/link";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { MoreHorizontal } from "lucide-react";
 
 interface Props {
     id: string;
@@ -24,7 +27,22 @@ export default function CardItem(props: Props) {
             </section>
             <div className="mt-3 flex flex-col items-center">
                 <span className="hidden sm:block text-ellipsis overflow-hidden whitespace-nowrap w-[120px] sm:w-auto text-gray-500">
-                    Dropdown here
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="ghost" className="h-8 w-8 p-0 rounded">
+                                <MoreHorizontal className="h-4 w-4 text-purple-300" />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end" className="rounded border-purple-500">
+                            {/* <DropdownMenuItem onClick={}> */}
+                            <DropdownMenuItem >
+                                Rename
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                Delete
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
                 </span>
             </div>
         </div>
