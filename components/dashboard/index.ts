97126ce0ea31498a2bc984eae246100b8100d50e
ColinAdo <colinadodev30@gmail.com>
2025-03-03 22:54:13 +0300

    Deleted timestamp component

--- components/dashboard/index.ts
+++ components/dashboard/index.ts
@@ -1,6 +1,5 @@
 export { default as CardItem } from "./CardItem";
 export { default as PageTitle } from "./PageTitle";
-export { default as Timestamp } from "./Timestamp";
 export { default as UploadFile } from "./UploadFile";
 export { default as FormDialog } from "./FormDialog";
 export { default as AnalyticBarChart } from "./AnalyticBarChart";
