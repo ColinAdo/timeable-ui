16d2219254d1d65d2f4c50d737799b5050effc2f
ColinAdo <colinadodev30@gmail.com>
2025-03-03 22:17:50 +0300

    Add id property

--- components/dashboard/GeneratedTimetable.tsx
+++ components/dashboard/GeneratedTimetable.tsx
@@ -24,6 +24,7 @@ export default function AnalyticCard() {
                 {timetableNames.slice(0, 6).map((d, i) => (
                     <CardItem
                         key={i}
+                        id={d.batch_id}
                         username="Colin"
                         create_at="12/Jan/2024"
                         timetableName={d.batch_id}
