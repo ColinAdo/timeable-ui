609fa7310ce49492ed35e1c3b6596cab5f67be27
ColinAdo <colinadodev30@gmail.com>
2025-03-03 22:26:35 +0300

    Add proper import

--- components/dashboard/index.ts
+++ components/dashboard/index.ts
@@ -1,9 +1,9 @@
 export { default as CardItem } from "./CardItem";
 export { default as PageTitle } from "./PageTitle";
+export { default as Timestamp } from "./Timestamp";
 export { default as UploadFile } from "./UploadFile";
 export { default as FormDialog } from "./FormDialog";
 export { default as AnalyticBarChart } from "./AnalyticBarChart";
 export { default as AnalyticLineChart } from "./AnalyticLineChart";
 export { default as GeneratedTimetable } from "./GeneratedTimetable";
 export { default as CreateTimetableForm } from "./CreateTimetableForm";
-export { default as Timestamp } from "./Timestamp";
