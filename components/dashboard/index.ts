243bd9b2c9aca4938308e2e09d7427f27db4c8a6
ColinAdo <colinadodev30@gmail.com>
2025-03-03 22:26:10 +0300

    Add Timestamp component

--- components/dashboard/index.ts
+++ components/dashboard/index.ts
@@ -6,3 +6,4 @@ export { default as AnalyticBarChart } from "./AnalyticBarChart";
 export { default as AnalyticLineChart } from "./AnalyticLineChart";
 export { default as GeneratedTimetable } from "./GeneratedTimetable";
 export { default as CreateTimetableForm } from "./CreateTimetableForm";
+export { default as Timestamp } from "./Timestamp";
