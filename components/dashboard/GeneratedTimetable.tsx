fbf52f406aa4ab33b54cd2fb1c8a8c5e277606b3
ColinAdo <colinadodev30@gmail.com>
2025-03-06 17:32:10 +0300

    Add proper function name

--- components/dashboard/GeneratedTimetable.tsx
+++ components/dashboard/GeneratedTimetable.tsx
@@ -10,7 +10,7 @@ import {
     CardContent,
 } from "@/components/ui/card";
 
-export default function AnalyticCard() {
+export default function GeneratedTimetable() {
     const { data: timetableNames } = useGetTimetableNamesQuery();
 
     if (!timetableNames) return null;
