77c7446540a62c59ca057650620cd78e7c459ecf
ColinAdo <colinadodev30@gmail.com>
2025-03-07 18:06:23 +0300

    Configure webSocketContext hook

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import { RequireAuth } from "@/components/utils";
 import { Navbar, Sidebar } from "@/components/common";
-// import { WebSocketProvider } from "@/hooks/WebSocketContext";
+import { WebSocketProvider } from "@/hooks/webSocketContext";
 
 interface Props {
   children: React.ReactNode;
@@ -9,15 +9,15 @@ interface Props {
 export default function Layout({ children }: Props) {
   return (
     <RequireAuth>
-      {/* <WebSocketProvider> */}
-      <Navbar />
-      <div className="flex bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-        <div className="hidden md:block">
-          <Sidebar />
+      <WebSocketProvider>
+        <Navbar />
+        <div className="flex bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+          <div className="hidden md:block">
+            <Sidebar />
+          </div>
+          <div className="p-5 w-full max-w-[1140px]">{children}</div>
         </div>
-        <div className="p-5 w-full max-w-[1140px]">{children}</div>
-      </div>
-      {/* </WebSocketProvider> */}
+      </WebSocketProvider>
     </RequireAuth>
   );
 }
---
+++ hooks/webSocketContext.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React, { createContext, useContext, ReactNode } from "react";
+import useWebSocket from "react-use-websocket";
+
+interface WebSocketContextType {
+    sendJsonMessage: (message: Record<string, any>) => void;
+    lastJsonMessage: Record<string, any> | null;
+    readyState: number;
+}
+
+// Create the WebSocket context
+const WebSocketContext = createContext<WebSocketContextType | undefined>(undefined);
+
+interface WebSocketProviderProps {
+    children: ReactNode;
+}
+
+export const WebSocketProvider: React.FC<WebSocketProviderProps> = ({ children }) => {
+    const WS_URL = `${process.env.NEXT_PUBLIC_WS_HOST}/api/v1/timetable/`;
+
+    const { sendJsonMessage, lastJsonMessage: rawLastJsonMessage, readyState } = useWebSocket(WS_URL, {
+        share: true,
+        shouldReconnect: () => true,
+    });
+
+    const lastJsonMessage = rawLastJsonMessage as Record<string, any> | null;
+
+    return (
+        <WebSocketContext.Provider value={{ sendJsonMessage, lastJsonMessage, readyState }}>
+            {children}
+        </WebSocketContext.Provider>
+    );
+};
+
+// Hook to use the WebSocketContext
+export const useWebSocketContext = (): WebSocketContextType => {
+    const context = useContext(WebSocketContext);
+    if (!context) {
+        throw new Error("useWebSocketContext must be used within a WebSocketProvider");
+    }
+    return context;
+};
