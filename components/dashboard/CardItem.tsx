086886cad4f67a2f549259467d4ed31717041ea6
ColinAdo <colinadodev30@gmail.com>
2025-03-08 15:38:48 +0300

    Add real time delet timetable

--- components/dashboard/CardItem.tsx
+++ components/dashboard/CardItem.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import { toast } from "sonner";
 import { useState } from "react";
 import { MoreHorizontal } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { RenameTimetable } from "@/components/dashboard";
+import { useWebSocketContext } from "@/hooks/webSocketContext";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import {
     DropdownMenu,
@@ -18,7 +20,19 @@ interface Props {
 }
 
 export default function CardItem(props: Props) {
+    const { sendJsonMessage } = useWebSocketContext();
     const [open, setOpen] = useState(false);
+
+    const onDelete = () => {
+        const sendData = {
+            batch_id: props.id,
+        }
+        sendJsonMessage({
+            event: "delete_timetable",
+            sendData,
+        });
+        toast.success("Timetable deleted successfully");
+    };
 
     return (
         <div className="flex flex-wrap justify-between gap-3">
@@ -44,7 +58,7 @@ export default function CardItem(props: Props) {
                         </DropdownMenuTrigger>
                         <DropdownMenuContent align="end" className="rounded border-purple-500">
                             <DropdownMenuItem onClick={() => setOpen(true)}>Rename</DropdownMenuItem>
-                            <DropdownMenuItem>Delete</DropdownMenuItem>
+                            <DropdownMenuItem onClick={onDelete}>Delete</DropdownMenuItem>
                         </DropdownMenuContent>
                     </DropdownMenu>
                 </span>
