b982c88dce7dc1a61f10273ad13b8585389d40cc
ColinAdo <colinadodev30@gmail.com>
2025-03-12 23:55:02 +0300

    Add proper import

--- components/forms/PasswordResetForm.tsx
+++ components/forms/PasswordResetForm.tsx
@@ -4,7 +4,12 @@ import { useResetPassword } from "@/hooks";
 import { Spinner } from "@/components/common";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent
+} from "@/components/ui/card";
 import {
   Form,
   FormItem,
