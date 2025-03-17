955f2032c3e73a1d33b8a6bfdc9716f22cdecb54
ColinAdo <colinadodev30@gmail.com>
2025-03-17 19:55:50 +0300

    Delete unwanted component

--- components/dashboard/index.ts
+++ components/dashboard/index.ts
@@ -3,6 +3,5 @@ export { default as PageTitle } from "./PageTitle";
 export { default as UploadFile } from "./UploadFile";
 export { default as FormDialog } from "./FormDialog";
 export { default as RenameTimetable } from "./RenameTimetable";
-export { default as AnalyticBarChart } from "./AnalyticBarChart";
 export { default as GeneratedTimetable } from "./GeneratedTimetable";
 export { default as CreateTimetableForm } from "./CreateTimetableForm";
