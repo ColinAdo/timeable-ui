3ee2beb8f43879235fdb3cbd5b9c277c47c12b48
ColinAdo <colinadodev30@gmail.com>
2025-03-18 23:41:02 +0300

    Add edit timetable schema

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -64,6 +64,15 @@ export const createTimetableSchema = z.object({
     prompt: z.string().optional(),
 });
 
+export const EditTimetableSchema = z.object({
+    rowId: z.string().min(1, "RowId is required."),
+    start_time: z.string().min(1, "Start time is required."),
+    end_time: z.string().min(1, "End time is required."),
+    day: z.string().min(1, "Day is required."),
+    unit_name: z.string().min(1, "Unit name is required."),
+    unit_code: z.string().min(1, "Unit code is required."),
+});
+
 export const renameSchema = z.object({
     name: z.string().min(1, "This field is required."),
 });
