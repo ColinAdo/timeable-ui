898a74d6ae1b9bcc88673c440d38a6739933ff53
ColinAdo <colinadodev30@gmail.com>
2025-03-21 12:32:50 +0300

    Add proper import

--- components/dashboard/index.ts
+++ components/dashboard/index.ts
@@ -3,6 +3,6 @@ export { default as PageTitle } from "./PageTitle";
 export { default as UploadFile } from "./UploadFile";
 export { default as FormDialog } from "./FormDialog";
 export { default as RenameTimetable } from "./RenameTimetable";
+export { default as EditTimetableForm } from "./EditTimetableForm";
 export { default as GeneratedTimetable } from "./GeneratedTimetable";
 export { default as CreateTimetableForm } from "./CreateTimetableForm";
-export { default as EditTimetableForm } from "./EditTimetableForm";
