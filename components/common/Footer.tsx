8fe4ec2408b2fd9cf1ed11c2acba09d42696674d
ColinAdo <colinadodev30@gmail.com>
2025-03-27 09:05:29 +0300

    Add proper padding

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -10,7 +10,7 @@ import {
 
 export default function Footer() {
   return (
-    <footer className="bg-black/[0.96] bg-grid-white/[0.02] backdrop-blur-md py-8">
+    <footer className="bg-black/[0.96] bg-grid-white/[0.02] backdrop-blur-md py-16">
       <div className="container mx-auto px-6">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
