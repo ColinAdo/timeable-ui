074f77c0930d5472f98d988797953b0293668217
ColinAdo <colinadodev30@gmail.com>
2025-03-27 09:41:51 +0300

    Add proper padding

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,7 +8,7 @@ import {
 
 export default function Page() {
   return (
-    <div className="flex flex-col gap-5 w-full mt-12">
+    <div className="flex flex-col gap-5 py-10 w-full mt-12">
       <PageTitle title="Dashboard" />
       <UploadFile />
       <section className="grid grid-cols-1 gap-4 lg:grid-cols-2 transition-all">
