838e8e1452783e887ded7fa24b80c30e5cc203bb
ColinAdo <colinadodev30@gmail.com>
2025-03-28 10:46:09 +0300

    Add proper styling

--- components/dashboard/GeneratedTimetable.tsx
+++ components/dashboard/GeneratedTimetable.tsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import { CardItem } from "@/components/dashboard/";
 import { useWebSocketContext } from "@/hooks/webSocketContext";
 import { useGetTimetableNamesQuery } from "@/redux/features/timetableSlice";
+import { useRetrieveUserQuery } from "@/redux/features/authApiSlice";
 import {
     Card,
     CardTitle,
@@ -13,6 +14,7 @@ import {
 } from "@/components/ui/card";
 
 export default function GeneratedTimetable() {
+    const { data: user } = useRetrieveUserQuery();
     const { lastJsonMessage } = useWebSocketContext();
     const { data: timetableNames, refetch } = useGetTimetableNamesQuery();
 
@@ -27,14 +29,14 @@ export default function GeneratedTimetable() {
             {timetableNames.length > 0 && (
                 <Card className="bg-black/[0.96] bg-grid-white[0.02] border-purple-400">
                     <CardHeader>
-                        <CardTitle>Generated Timetables</CardTitle>
+                        <CardTitle>Your Generated Timetables</CardTitle>
                     </CardHeader>
                     <CardContent>
                         {timetableNames.slice(0, 6).map((d, i) => (
                             <CardItem
                                 key={i}
                                 id={d.batch_id}
-                                username="Colin"
+                                username={user?.username[0] || "U"}
                                 timetableName={d.name}
                             />
                         ))}
