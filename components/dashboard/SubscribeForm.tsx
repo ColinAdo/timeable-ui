0ed47b16a029b43b417b34bc004ed0d036072a94
ColinAdo <colinadodev30@gmail.com>
2025-04-01 13:57:57 +0300

    Add proper import

--- components/dashboard/SubscribeForm.tsx
+++ components/dashboard/SubscribeForm.tsx
@@ -8,6 +8,7 @@ import {
     FormControl,
     FormMessage,
 } from "@/components/ui/form";
+import * as z from "zod";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { Input } from "@/components/ui/input";
@@ -18,7 +19,6 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { useWebSocketContext } from "@/hooks/webSocketContext";
 import { useSubscribeMutation } from "@/redux/features/timetableSlice";
 import { toast } from "sonner";
-import * as z from "zod";
 import { Spinner } from "../common";
 
 
